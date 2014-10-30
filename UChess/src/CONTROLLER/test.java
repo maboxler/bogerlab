e6a72fbc20b59187a7e9c2c257b9d91fbe8750e5
maboxler <maboxler@3DLAB5.htwg-konstanz.de>
2014-10-30 15:00:44 +0100

    überzeuge mich :)

--- UChess/src/CONTROLLER/test.java
+++ UChess/src/CONTROLLER/test.java
@@ -3,7 +3,7 @@ package CONTROLLER;
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Git hat noch nicht überzeugt !!!!");
 
 	}
 

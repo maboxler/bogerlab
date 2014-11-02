12d1e56915c993034810ff8412678947bcc366d5
Tarek <Tarek@8.21.2.50>
2014-11-02 09:19:49 +0100

    Chesspieces added

---
+++ UChess/src/VIEW/Knight.java
@@ -0,0 +1,11 @@
+package VIEW;
+
+public class Knight extends Chesspiece {
+
+	@Override
+	boolean validMove(int x, int y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ UChess/src/VIEW/Tower.java
@@ -0,0 +1,11 @@
+package VIEW;
+
+public class Tower extends Chesspiece {
+
+	@Override
+	boolean validMove(int x, int y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}

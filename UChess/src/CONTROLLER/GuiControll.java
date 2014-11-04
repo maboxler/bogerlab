bd44d40a137c7fba255dff50b518e9b2694407cf
Tarek <Tarek@172.20.133.141>
2014-11-04 10:53:18 +0100

    "GuiController

---
+++ UChess/src/CONTROLLER/GuiControll.java
@@ -0,0 +1,13 @@
+package CONTROLLER;
+
+public class GuiControll {
+	private int field[][];
+	
+	public GuiControll() {
+		
+	}
+	
+	public boolean checkMove(int x1,int y1,int x2,int y2) {
+		return false;
+	}
+}

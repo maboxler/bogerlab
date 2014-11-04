1b79e51b816e9a28a5f9bbf3456dc49370d1b42c
Tarek <Tarek@172.20.133.141>
2014-11-04 11:54:02 +0100

    Seperate field class

--- UChess/src/CONTROLLER/GuiControll.java
+++ UChess/src/CONTROLLER/GuiControll.java
@@ -7,47 +7,14 @@ public class GuiControll {
 	private Chesspiece gamefield[][];
 	
 	public GuiControll() {
-		gamefield = new Chesspiece[8][8];
-		initGameField();
 		
 	}
 	
 	public static void main(String[] args) {
-		initGameField();
-	}
-	
-	private static void initGameField() {
-		initPawns();
-		initTowers();
-		initKnights();
-		initBishops();
-		intiKings();
-		initQueens();
-	}
-	
-	private static void initPawns(){
 		
 	}
 	
-	private static void initTowers(){
-		
-	}
 	
-	private static void initKnights(){
-		
-	}
-	
-	private static void initBishops(){
-		
-	}
-	
-	private static void initQueens(){
-		
-	}
-	
-	private static void intiKings(){
-		
-	}
 	
 	public boolean checkMove(int x1,int y1,int x2,int y2) {
 		return false;

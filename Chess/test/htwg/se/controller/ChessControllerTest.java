07e5f721a551c7a2295c1a44819d4745bcab9a06
tabuechn <tabuechn@htwg-konstanz.de>
2015-01-06 20:13:30 +0100

    controller test added

--- Chess/test/htwg/se/controller/ChessControllerTest.java
+++ Chess/test/htwg/se/controller/ChessControllerTest.java
@@ -29,6 +29,15 @@ public class ChessControllerTest {
 		cs.move(new Point(1, 3),new Point(1,0));
 		cs.move(new Point(1, 0),new Point(3,0));
 		cs.move(new Point(3, 3),new Point(3,4));
+		assertEquals("It's blacks turn", cs.getStatusMessage());
+		assertEquals(cs.getField()[0][1].getChessPiece().toChar(),'P');
+		cs.move(new Point(1, 7),new Point(0,5));
+		assertEquals("It's whites turn", cs.getStatusMessage());
+		assertNull(cs.getField()[1][7].getChessPiece());
+		assertEquals(cs.getField()[0][5].getChessPiece().toChar(),'H');
+		cs.move(new Point(1,0), new Point(0,2));
+		assertNull(cs.getField()[1][0].getChessPiece());
+		assertEquals(cs.getField()[0][2].getChessPiece().toChar(),'H');
 	}
 
 }

eb3130552929b25a434efe8aff7b68a466a423c7
tabuechn <tabuechn@htwg-konstanz.de>
2015-01-07 12:48:07 +0100

    controller fully tested

--- Chess/test/htwg/se/controller/ChessControllerTest.java
+++ Chess/test/htwg/se/controller/ChessControllerTest.java
@@ -30,15 +30,33 @@ public class ChessControllerTest {
 		cs.move(new Point(1, 0),new Point(3,0));
 		cs.move(new Point(3, 3),new Point(3,4));
 		assertEquals("It's blacks turn", cs.getStatusMessage());
+		assertEquals(cs.getField()[0][0].getChessPiece().toChar(), 'T');
+		cs.move(new Point(0,0), new Point(0,1));
+		assertEquals(cs.getField()[0][0].getChessPiece().toChar(), 'T');
 		assertEquals(cs.getField()[0][1].getChessPiece().toChar(),'P');
 		assertEquals(cs.getField()[1][7].getChessPiece().toChar(), 'H');
 		cs.move(new Point(1, 7),new Point(0,5));
 		assertEquals("It's whites turn", cs.getStatusMessage());
+		assertEquals(cs.getField()[0][7].getChessPiece().toChar(), 'T');
+		assertEquals(cs.getField()[0][6].getChessPiece().toChar(), 'P');
+		cs.move(new Point(0,7),new Point(0,6));
+		assertEquals(cs.getField()[0][7].getChessPiece().toChar(), 'T');
+		assertEquals(cs.getField()[0][6].getChessPiece().toChar(), 'P');
 		assertNull(cs.getField()[1][7].getChessPiece());
 		assertEquals(cs.getField()[0][5].getChessPiece().toChar(),'H');
 		cs.move(new Point(1,0), new Point(0,2));
 		assertNull(cs.getField()[1][0].getChessPiece());
 		assertEquals(cs.getField()[0][2].getChessPiece().toChar(),'H');
+	}
+	
+	@Test
+	public void moveTest2() {
+		assertEquals("It's blacks turn", cs.getStatusMessage());
+		assertEquals(cs.getField()[1][7].getChessPiece().toChar(),'H');
+		assertNull(cs.getField()[7][4].getChessPiece());
+		cs.move(new Point(1,7), new Point(7,4));
+		assertEquals(cs.getField()[1][7].getChessPiece().toChar(),'H');
+		assertNull(cs.getField()[7][4].getChessPiece());
 	}
 
 }
